cb9cd94c14f13441ec64b3e054eb9d10d491b16e
CCleaner <ggcoolg@yandex.ru>
2022-08-28 22:53:07 +0300

    add get_users method

--- apps/users/router.py
+++ apps/users/router.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, status
+from .services import _get_users
+from .schemas import UsersRequest
 
 
 router = APIRouter(
@@ -9,6 +11,13 @@ router = APIRouter(
 @router.post(
     path = "/parse",
     status_code = status.HTTP_200_OK)
-async def get_users():
+async def get_users(
+    request: UsersRequest
+):
     """ Endpoint to get FunPay users
     """
+
+    response = await _get_users(
+        value = request)
+
+    return response
